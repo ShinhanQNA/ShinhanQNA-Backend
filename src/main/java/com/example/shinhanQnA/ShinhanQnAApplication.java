a957591d5bfc02f7a83165b90e458d1e6e449556
bindoong01 <mailsky9687@gmail.com>
2025-07-31 03:52:18 +0900

    초기 설정

---
+++ src/main/java/com/example/shinhanQnA/ShinhanQnAApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinhanQnA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanQnAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinhanQnAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shinhanQnA/ShinhanQnAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shinhanQnA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhanQnAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
